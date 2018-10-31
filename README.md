2ba568c60acf216b69c0545cfe3ebe9c4483e9f7
mhe70 <mhe70@bloomberg.net>
2018-10-31 11:48:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Exchange-Example
+This is an example DApp to interact with the exchange contracts.
+
+## Install
+`npm install`
+
+## To Perform Exchange Operations
+
+1. Make copy of `.env.example` file and rename to `.env`.
+1. Populate `USER_ADDRESS` and `USER_ADDRESS_PRIVATE_KEY` in `.env`. This is the account where the transactions will be made from. `USER_ADDRESS_PRIVATE_KEY` need to be provided without '0x' prefix.
+2. Use provided scripts for making transactions:
+
+    - Buy CLC with ETH: `node scripts/BuyCLCWithETH.js`
+    - Sell CLC for ETH: `node scripts/SellCLCWithETH.js`
+    - Cancel Order: `node scripts/CancelOrder.js`. Need to modify `orderId` variable in script.
---
+++ scripts/BuyCLCWithETH.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const Web3 = require('web3')
+const Web3Utils = require('web3-utils')
+const assert = require('assert');
+const { sendTx, sendRawTx, getReceipt } = require('../helper/sendTx')
+
+const ERC827 = require('../abis/ERC827.abi')
+const Exchange = require('../abis/Exchange.abi')
+
+const {
+  USER_ADDRESS,
+  USER_ADDRESS_PRIVATE_KEY,
+  EXCHANGE_ADDRESS,
+  ETH_TOKEN_ADDRESS,
+  RPC_URL,
+  GAS_PRICE
+} = process.env
+
+const CLC_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+const web3Provider = new Web3.providers.HttpProvider(RPC_URL)
+const web3Instance = new Web3(web3Provider)
+
+const erc827 = new web3Instance.eth.Contract(ERC827, ETH_TOKEN_ADDRESS)
+const exchange = new web3Instance.eth.Contract(Exchange, EXCHANGE_ADDRESS)
+
+const tradeTokenAmount = 0.001
+const tradePrice = 2
+const baseTokenAmount = tradeTokenAmount * tradePrice
+
+async function main() {
+  const chainId = await sendRawTx({
+    url: RPC_URL,
+    params: [],
+    method: 'net_version'
+  })
+  
+  let nonce = await sendRawTx({
+    url: RPC_URL,
+    method: 'eth_getTransactionCount',
+    params: [USER_ADDRESS, 'latest']
+  })
+  nonce = Web3Utils.hexToNumber(nonce)
+  
+  const buyData = await exchange.methods
+      .buy(ETH_TOKEN_ADDRESS, CLC_ADDRESS, USER_ADDRESS, Web3Utils.toWei(tradeTokenAmount.toString()), Web3Utils.toWei(tradePrice.toString()))
+      .encodeABI({ from: USER_ADDRESS })
+
+  const txData = await erc827.methods
+      .approveAndCall(EXCHANGE_ADDRESS, Web3Utils.toWei(baseTokenAmount.toString()), buyData)
+      .encodeABI({ from: USER_ADDRESS })
+
+  try {
+    const txHash = await sendTx({
+      rpcUrl: RPC_URL,
+      privateKey: USER_ADDRESS_PRIVATE_KEY,
+      data: txData,
+      nonce,
+      gasPrice: '1',
+      amount: '0',
+      gasLimit: 500000,
+      to: ETH_TOKEN_ADDRESS,
+      web3: web3Instance,
+      chainId: chainId
+    })
+    
+    const txReceipt = await getReceipt(txHash, RPC_URL)
+    if (txReceipt.status == '0x1') {
+      console.log('transaction succeeded: ', txHash)
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main()
